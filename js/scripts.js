ddb733d3670a4a57cc139647cb5f3b0bf380068c
John Pork <John@Pork.pl>
2023-05-15 13:58:12 +0200

    refactor

--- js/scripts.js
+++ js/scripts.js
@@ -1,27 +1,33 @@
 'use strict';
 
-const getData = () => {
-    
-    fetch('http://api.nbp.pl/api/exchangerates/tables/a/?format=json')
-    .then(reponse => reponse.json())
-    .then(result =>{
+const baseUrl = 'http://api.nbp.pl/api/exchangerates/tables/'
 
-        console.log(result);
+const getCurrency = async (table)  => {
+    try {
+        const response = await fetch(`${baseUrl}${table}`);
+        const data = await response.json();
+        return data;
+    }
+    catch(err){
+        console.log(`To jest błąd: ${err}`);
+    }
+}
 
-        console.log(result[0].rates);
-        result[0].rates.forEach((elem, i) => {
-            const tr = document.createElement('tr');
+getCurrency('a').then((data) => {
+
+    console.log(data)
+
+    const [currencies] = data;
+
+    const { rates } = currencies;
+
+    rates.forEach((elem, i) => {
+        const tr = document.createElement('tr');
             tr.innerHTML = 
             `<td>${++i}</td>
              <td>${elem.currency}</td>
              <td>${elem.mid}</td>
             `
             document.querySelector('table tbody').appendChild(tr);
-        })
-        
     });
-}
-
-const btn = document.querySelector('button');
-
-btn.addEventListener('click', getData)
+});
